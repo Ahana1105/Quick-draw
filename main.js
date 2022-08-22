fffe8e6ec06b12fdd634c1273955b44ca566adff
Ahana1105 <77888005+Ahana1105@users.noreply.github.com>
2022-08-22 21:15:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var draw_score= 0;
+var draw_timer= 0;
+var timer_check="";
+var answer_holder="";
+var drawn_sketch ="";
+
 function setup() {
     canvas = createCanvas(280, 240)
     canvas.center();
@@ -6,13 +12,18 @@ function setup() {
     synth = window.speechSynthesis;
 }
 
-function clear_canvas() {
-    background("white");
-}
 
 function preload() {
     classifier = ml5.imageClassifier('DoodleNet');
 }
+
+function clear_canvas() {
+    background("white");
+    random_number=Math.floor((Math.random() * quick_draw_data_set[random_number]))
+    sketch="quick_draw_data_set[random_number];"
+    document.getElementById('sketch_drawn').innerHTML = 'Sketch To be Drawn: ' + sketch;
+}
+
 
 function draw() {
     strokeWeight(10);
@@ -25,20 +36,38 @@ function draw() {
 function classifyCanvas() {
     classifier.classify(canvas, gotResult);
 }
-
 function gotResult(error, results) {
-
     if (error) {
-        console.error(error);
-    } else {
-        console.log(results);
-        document.getElementById("label").innerHTML = "label: " + results[0].label;
-        confidence = results[0].confidence*100;
-        confidence = Math.round(confidence) + "%"
-        document.getElementById("confidence").innerHTML = "confidence: " + confidence;
+      console.error(error);
+    }
+    console.log(results);
+    drawn_sketch = results[0].label;
+    document.getElementById('your_sketch').innerHTML = 'Your Sketch: ' + drawn_sketch;
+  
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+  }
 
-        utterThis = new SpeechSynthesisUtterance(results[0].label);
-        synth.speak(utterThis)
+ 
+
+
+
+function check_sketch()
+{
+  draw_timer++;
+  document.getElementById('timer').innerHTML = 'Timer: ' + draw_timer;
+  console.log(draw_timer)
+  if(draw_timer > 400)
+    {
+      draw_timer = 0;
+      timer_check = "completed"
+    }
+    if(timer_check =="completed" || answer_holder == "set")
+    {
+      timer_check = "";
+      answer_holder = "";
+      updateCanvas();
     }
 
-} 
+}
+
+
